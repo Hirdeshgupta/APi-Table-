5bdcbb7c902ede563eaa280ea7e4932af0d78ffa
Hirdeshgupta <hirdeshgupta16@gmail.com>
2020-06-16 13:58:44 -0700

    Search added

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ fetch("https://api.covid19api.com/summary")
     .then(data=>data.json())
     .then(
         (datajson)=>{
-            console.log(datajson.Countries)
             datajson.Countries.forEach(element => {
                 let tr=document.createElement("TR");
                 Object.values(element).forEach(x => {
@@ -21,5 +20,17 @@ fetch("https://api.covid19api.com/summary")
                 tr.appendChild(NodeElement);
             })
             document.querySelector("thead").appendChild(tr);
+            document.querySelector("input").addEventListener("keydown",()=>{
+                let search =document.querySelector("input").value;
+                let regex = new RegExp(search,"ig")
+                document.querySelectorAll("tbody tr").forEach(element=>{
+                    if(!((element.children[0].textContent.match(regex)))){
+                        element.style.display="none"
+                    }
+                    else{
+                        element.style.display="table-row"
+                    }
+                })
+            })
         }
     )
